caf6aad1bc7b8a31890f58dbb7d80b35745bb953
Linus-llm <linusmolgaard@gmail.com>
2025-03-12 13:23:35 +0100

    Updated taskOne and created taskTwo

--- Opgaverne/src/TaskOne/Customer.java
+++ Opgaverne/src/TaskOne/Customer.java
@@ -1,21 +1,23 @@
 package TaskOne;
 
 public class Customer { //1.a class customer created
-    private String firstName;
-    private String lastName;
-    private String username;
-    private int ID;
-    private static int counter;
-    public Customer(String firstName,String lastName, String username){
+    private String firstName; //1.d
+    private String lastName; //1.d
+    private String username; //1.d
+    private int ID; //1.d
+    private static int counter; //1.d
+    public Customer(String firstName,String lastName, String username){ //1.b Constructor that takes the name and username as parameters
         this.firstName = firstName;
         this.username = username;
         this.lastName = lastName;
-        counter += 1;
-         ID = counter;
+        counter += 1; //1.c counter gets increased each time the constructor is called
+         ID = counter; //1.c using counter to initialize ID
     }
-    public String toString(){
+    public String toString(){ //toString method to print the variables nice and clean
        return "first name: " +this.firstName+" | last name: "+this.lastName+" | username: "+this.username+" | ID: "+this.ID+" | counter: "+counter +"\n";
     }
+
+    //1.d Getters and setters
     public int getID(){
         return this.ID;
     }
@@ -31,5 +33,20 @@ public class Customer { //1.a class customer created
 
     public static int getCounter() {
         return counter;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+
+    }
+    public void setUsername(String username){
+        this.username = username;
     }
 }
--- Opgaverne/src/TaskOne/Main.java
+++ Opgaverne/src/TaskOne/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Customer C1 = new Customer("Linus","Molgaard","lmol");
         Customer C2 = new Customer("Nikoo","Mars","nars");
         Customer C3 = new Customer("Tess","Sett","test");
-
+        //1.e  creating arraylist type Customer and adding the instances to the list
         ArrayList<Customer> customers = new ArrayList<>();
         customers.add(C1);
         customers.add(C2);
@@ -15,6 +15,7 @@ public class Main {
         printCustomers(customers);
 
     }
+    //1.f creating method to print out hte customers in the customers arraylist
     public static void printCustomers(ArrayList<Customer> d){
         for(Customer i: d){
             System.out.println(i);
